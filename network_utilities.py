3afe4d72aa3e9649f039ed2242cc497416cd02ac
nicoloval <nico.vallarano@gmail.com>
2019-10-15 15:28:10 +0200

    added utilities file with basic network functions

---
+++ network_utilities.py
@@ -0,0 +1,30 @@
+import numpy as np
+import scipy.sparse
+
+def out_degree(a):
+    # todo: for out_degree and in_degree...check np.int32 is always returned
+    """returns matrix A out degrees
+
+    :param a: numpy.ndarray, a matrix
+    :return: numpy.ndarray
+    """
+    # if the matrix is a numpy array
+    if type(a) == np.ndarray:
+        return np.sum(a > 0, 1)
+    # if the matrix is a scipy sparse matrix
+    elif type(a) in [scipy.sparse.csr.csr_matrix, scipy.sparse.coo.coo_matrix]:
+        return np.sum(a > 0, 1).A1
+
+
+def in_degree(a):
+    """returns matrix A in degrees
+
+    :param a: np.ndarray, a matrix
+    :return: numpy.ndarray
+    """
+    # if the matrix is a numpy array
+    if type(a) == np.ndarray:
+        return np.sum(a > 0, 0)
+    # if the matrix is a scipy sparse matrix
+    elif type(a) in [scipy.sparse.csr.csr_matrix, scipy.sparse.coo.coo_matrix]:
+        return np.sum(a > 0, 0).A1
