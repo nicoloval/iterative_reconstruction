8b3d7d7baaad0338ffd5d8e4567d7770fd7ffb22
nicoloval <nico.vallarano@gmail.com>
2019-10-16 02:14:03 +0200

    added degree expectation under dcm

--- network_utilities.py
+++ network_utilities.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.sparse
+from numba import jit
 
 def out_degree(a):
     # todo: for out_degree and in_degree...check np.int32 is always returned
@@ -28,3 +29,65 @@ def in_degree(a):
     # if the matrix is a scipy sparse matrix
     elif type(a) in [scipy.sparse.csr.csr_matrix, scipy.sparse.coo.coo_matrix]:
         return np.sum(a > 0, 0).A1
+
+@jit(nopython=True)
+def expected_out_degree(sol):
+    # TODO: controllare che funzioni
+    # TODO: if par is not given call setting()
+    """returns expected out degree after ERGM method, on undirected networks
+    returns just the expected degree
+
+    Parameters
+    ----------
+
+    :param sol: :class:`~numpy.ndarray`
+        Solution of the ERGM problem
+
+    Returns
+    -------
+
+    :return k: :class:`~numpy.ndarray`
+        array of expected out-degrees
+
+    """
+    n = int(sol.size / 2)
+    a_out = sol[0:n]
+    a_in = sol[n:]
+
+    k = np.zeros(n)  # allocate k
+    for i in range(n):
+        for j in range(n):
+            if i != j:
+                k[i] += a_in[j]*a_out[i] / (1 + a_in[j]*a_out[i])
+
+    return k
+
+
+@jit(nopython=True)
+def expected_in_degree(sol):
+    """returns expected in degree after ERGM method, on undirected networks
+    returns just the expected degree
+
+    Parameters
+    ----------
+
+    :param sol: :class:`~numpy.ndarray`
+        Solution of the ERGM problem
+
+    Returns
+    -------
+
+    :return k: :class:`~numpy.ndarray`
+        array of expected in-degrees
+    """
+    n = int(sol.size/2)
+    a_out = sol[0:n]
+    a_in = sol[n:]
+    k = np.zeros(n)  # allocate k
+    for i in range(n):
+        for j in range(n):
+            if i != j:
+                k[i] += a_in[i]*a_out[j]/(1 + a_in[i]*a_out[j])
+
+    return k
+
