f42772d08c72460f216617d0af30d47474626fc5
Tiziano Squartini <tsquar@consensus.business.uzh.ch>
2019-10-28 11:46:00 +0100

    fixed experiment on dcm_rd

--- experiments/btc_reconstruction_dcmrd.py
+++ experiments/btc_reconstruction_dcmrd.py
@@ -8,11 +8,11 @@ import sys
 import os
 sys.path.append('../')
 import sample # where the functions are
-import network_utilities as utils
 import numpy as np
 import scipy.sparse
 import networkx as nx
 import time
+import datetime
 
 # btc database absolute path
 db_path = '/mnt/hdd_data/imt/NET-btc530k-heur_2s-week/'
@@ -35,19 +35,20 @@ sol, step, diff = sample.iterative_solver(A, eps=1e-04, method='dcm_rd')
 t = time.time() - tic
 
 # test solution
-expected_out_it = utils.expected_out_degree(sol, 'dcm_rd')
-expected_in_it = utils.expected_in_degree(sol, 'dcm_rd')
+d = sample.scalability_classes(A, 'dcm_rd')
+expected_out_it = sample.expected_out_degree(sol, 'dcm_rd', d)
+expected_in_it = sample.expected_in_degree(sol, 'dcm_rd', d)
 expected_k = np.concatenate((expected_out_it, expected_in_it))
-k_out = utils.out_degree(A)
-k_in = utils.in_degree(A)
+k_out = sample.out_degree(A)
+k_in = sample.in_degree(A)
 k = np.concatenate((k_out, k_in))
-
+error = max(abs(k - expected_k))
 result = np.allclose(expected_k, k, atol=1e-02, rtol=1e-02)
 
 # output
 print('experiment success: {}'.format(result))
 if not result:
-    print('error = {}'.format(max(abs(k - expected_k))))
+    print('error = {}'.format(error))
 print('diff = {}'.format(diff) )
 print('steps = {}'.format(step) )
 print('time = {}'.format(t) )
@@ -55,13 +56,13 @@ print('time = {}'.format(t) )
 # save results
 csvfile = 'df_btc_dcmrd.csv'
 # if the csv file already exists, do not write the header
-head_line = 'experiment_date,file,succes,error,diff,steps,time'
+head_line = 'experiment_date,file,succes,error,diff,steps,time\n'
 if not os.path.isfile(csvfile):
-    with open(csv_file, 'w') as cfile:
+    with open(csvfile, 'w') as cfile:
         cfile.write(head_line)
 
 date = datetime.date.today()
 # writing the df
 line = '{},{},{},{},{},{},{}\n'.format(date, week, result, error, diff, step, t )
-with open(csv_file, 'a') as cfile:
+with open(csvfile, 'a') as cfile:
     cfile.write(line)
